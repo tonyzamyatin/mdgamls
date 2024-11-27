708acc27bf3977d5ddd4dc9858725e7255d4b7bb
20hannah03 <e12120508@student.tuwien.ac.at>
2024-11-27 15:21:16 +0100

    part 1 adapted

--- UE5/part1.py
+++ UE5/part1.py
@@ -1,22 +1,15 @@
-from pathlib import Path
-
+import copy
 import pandas as pd
 
 
-def read_csv(file: Path) -> pd.DataFrame:
+def read_csv(file: str) -> pd.DataFrame:
     """Reads data from a csv file and returns the data as a Pandas dataframe"""
-    # TODO: Implement
+    return pd.read_csv(file)
 
 
 def clean_data(data_frame: pd.DataFrame) -> pd.DataFrame:
     """Returns a clean copy of the data frame"""
-    # TODO: Implement, use copy.deepcopy to first copy the dataframe before cleaning as not to overwrite the original data
-
-data = pd.read_csv('UE5-data/data_ehr.csv')
-rows, columns = data.shape
-print('Rows:', rows, 'Columns:', columns)
-threshold = len(data) * 0.30
-data_cleaned = data.dropna(axis=1, thresh=threshold)
-rows_cleaned, columns_cleaned = data_cleaned.shape
-print('Rows_cleaned:', rows_cleaned, 'Columns_cleaned:', columns_cleaned)
-print(data_cleaned.info())
+    data_copy = copy.deepcopy(data_frame)
+    thresh = len(data_copy) * 0.30
+    thresh = int(thresh)
+    return data_copy.dropna(axis=1, thresh=thresh)
